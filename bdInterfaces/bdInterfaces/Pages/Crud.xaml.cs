c3d90c3d8cb0df3b2593ed9248203b1ffac61bc9
MrHarvyson <josecmoreno3d@gmail.com>
2023-01-24 17:43:51 +0100

    paginas inicio

---
+++ bdInterfaces/bdInterfaces/Pages/Crud.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace bdInterfaces.Pages;
+
+public partial class Crud : Page
+{
+    public Crud()
+    {
+        InitializeComponent();
+    }
+}
---
+++ bdInterfaces/bdInterfaces/Pages/Inicio.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace bdInterfaces.Pages;
+
+public partial class Inicio : Page
+{
+    public Inicio()
+    {
+        InitializeComponent();
+    }
+
+    
+}
